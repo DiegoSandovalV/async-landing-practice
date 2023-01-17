ef3b0406284870b6610c0cf593f3f424f0d82b58
diego <sandoval.vargas.diego.1c@gmail.com>
2023-01-16 21:42:22 -0600

    Api Use

---
+++ src/assets/main.js
@@ -0,0 +1,42 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCgQna2EqpzqzfBjlSmzT72w&part=snippet%2Cid&order=date&maxResults=9'
+
+const content = document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b296d75163mshca7af93ea3c9630p112b82jsnd2fd6921b99b',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlAPI){
+    const response = await fetch(urlAPI, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try{
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+            <div class="group relative">
+                <div
+                    class="w-full bg-gray-200 aspect-2-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description} class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                    </h3>
+                </div>
+            </div>
+        `).slice(0,4).join('')}
+        `;
+        content.innerHTML = view;
+    }catch (error){
+        console.log(error);
+    }
+})();
